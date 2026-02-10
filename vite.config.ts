0f1b55935033df25e503d9557ab8a6fdb548186f
nang818 <77950741+nang818@users.noreply.github.com>
2026-02-11 00:02:55 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,28 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import path, { resolve } from 'path' // resolve를 여기서 가져와야 합니다!
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/work_archive/', // 깃허브 저장소 이름을 적어주면 빌드 시 경로를 다 맞춰줍니다.
+  base: '/work_archive/',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // 서브페이지를 아래와 같이 하나씩 등록해줘야 dist에 생성됩니다.
+        // public 폴더를 경로에서 제외하고 등록해보세요. 
+        // 만약 public 안에 계속 두실 거라면 아래 경로를 유지하세요.
         'dm-bnr-1': resolve(__dirname, 'public/works/2020/dm-bnr-1.html')
       }
     }
   },
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
